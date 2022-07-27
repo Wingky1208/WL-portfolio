4f0566126e88867b8b435b5e67f3e8b9085a7049
liangying <wingkyleung1208@hotmail.com>
2022-07-27 19:04:43 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import './style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
