3c7ef6f7b3d5c70d34a2eb9c87b6bf74ddecd99d
liangying <wingkyleung1208@hotmail.com>
2022-07-27 20:25:04 -0400

    de

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar2 from './components/Nav';
 import Footer from './components/Footer';
@@ -7,7 +7,8 @@ import About from './components/Page/About';
 import Contact from './components/Page/Contact';
 import Porfolio from './components/Page/Porfolio';
 import Resume from './components/Page/Resume';
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+
 
 import './App.css';
 
@@ -15,26 +16,26 @@ function App() {
 
 
   return (
-    <Router>
+    <HashRouter basename='/'>
       <div>
         <Header />
         <Navbar2 />
 
 
 
-        <Routes>
-          <Route path="/WL-portfolio" element={<Home />} />
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/porfolio' element={<Porfolio />} />
-          <Route exact path='/resume' element={<Resume />} />
-          <Route exact path='/contact' element={<Contact />} />
-          <Route exact path='/contact' element={<Contact />} />
 
-        </Routes>
+        <Route path="/WL-portfolio" element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/porfolio' element={<Porfolio />} />
+        <Route exact path='/resume' element={<Resume />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/contact' element={<Contact />} />
+
+
 
         <Footer />
       </div>
-    </Router>
+    </HashRouter>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import './style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from 'react-router-dom';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+
+    <App />
+
   </React.StrictMode>
 );
 
